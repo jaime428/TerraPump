39d1366c73c58b77ad78cf701498468f54045fea
jaime428 <154565618+jaime428@users.noreply.github.com>
2025-07-19 02:46:38 -0400

    Update firebase_config.py

--- app/firebase_config.py
+++ app/firebase_config.py
@@ -3,6 +3,9 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import pyrebase
 
+#debug line
+st.write("Secrets loaded:", st.secrets["firebase"])
+
 # --- Admin SDK (used for Firestore) ---
 if not firebase_admin._apps:
     cred = credentials.Certificate(dict(st.secrets["firebase"]))
