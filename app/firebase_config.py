f337209b5b074627c62965dbb43ab5b9de1322e5
jaime428 <154565618+jaime428@users.noreply.github.com>
2025-07-19 02:24:01 -0400

    Update firebase_config.py

--- app/firebase_config.py
+++ app/firebase_config.py
@@ -1,12 +1,25 @@
 import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, firestore
+import pyrebase
 
+# --- Admin SDK (used for Firestore) ---
+if not firebase_admin._apps:
+    cred = credentials.Certificate(dict(st.secrets["firebase"]))
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+# --- Pyrebase client-side config (used for login/auth) ---
 firebase_config = {
-    "apiKey": st.secrets["firebase"]["apiKey"],
-    "authDomain": st.secrets["firebase"]["authDomain"],
-    "databaseURL": st.secrets["firebase"]["databaseURL"],
+    "apiKey": st.secrets["firebase_apiKey"],
+    "authDomain": st.secrets["firebase_authDomain"],
     "projectId": st.secrets["firebase"]["project_id"],
-    "storageBucket": st.secrets["firebase"]["storageBucket"],
-    "messagingSenderId": st.secrets["firebase"]["messagingSenderId"],
-    "appId": st.secrets["firebase"]["appId"],
-    "measurementId": st.secrets["firebase"]["measurementId"]
+    "storageBucket": st.secrets["firebase_storageBucket"],
+    "messagingSenderId": st.secrets["firebase_messagingSenderId"],
+    "appId": st.secrets["firebase_appId"],
+    "measurementId": st.secrets["firebase_measurementId"]
 }
+
+firebase = pyrebase.initialize_app(firebase_config)
+auth = firebase.auth()
